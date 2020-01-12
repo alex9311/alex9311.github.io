e177f1477d75367629fcc7cc6878f0ebfc3f47ad
Alex Simes <alex.simes29@gmail.com>
2020-01-11 21:33:20 -0800

    oops forgot to push updated pose estimation inference

--- code-projects/pose-estimation/inference.py
+++ code-projects/pose-estimation/inference.py
@@ -26,155 +26,23 @@ from config import update_config
 from core.function import get_final_preds
 from utils.transforms import get_affine_transform
 
-COCO_KEYPOINT_INDEXES = {
-    0: 'nose',
-    1: 'left_eye',
-    2: 'right_eye',
-    3: 'left_ear',
-    4: 'right_ear',
-    5: 'left_shoulder',
-    6: 'right_shoulder',
-    7: 'left_elbow',
-    8: 'right_elbow',
-    9: 'left_wrist',
-    10: 'right_wrist',
-    11: 'left_hip',
-    12: 'right_hip',
-    13: 'left_knee',
-    14: 'right_knee',
-    15: 'left_ankle',
-    16: 'right_ankle'
-}
-
-COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
-
-
-def get_person_detection_boxes(model, cv2_img, threshold=0.5):
-    img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    img = Image.fromarray(img)  # Load the image
-    transform = transforms.Compose([transforms.ToTensor()])  # Defing PyTorch Transform
-    img = transform(img)  # Apply the transform to the image
-    pred = model([img])  # Pass the image to the model
-    pred_classes = [COCO_INSTANCE_CATEGORY_NAMES[i]
-                    for i in list(pred[0]['labels'].numpy())]  # Get the Prediction Score
-    pred_boxes = [[(i[0], i[1]), (i[2], i[3])]
-                  for i in list(pred[0]['boxes'].detach().numpy())]  # Bounding boxes
-    pred_score = list(pred[0]['scores'].detach().numpy())
-    # Get list of index with score greater than threshold.
-    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
-    pred_boxes = pred_boxes[:pred_t+1]
-    pred_classes = pred_classes[:pred_t+1]
-    for idx, box in enumerate(pred_boxes):
-        if pred_classes[idx] == 'person':
-            cv2.rectangle(cv2_img, box[0], box[1], color=(0, 255, 0),
-                          thickness=3)  # Draw Rectangle with the coordinates
-            cv2.putText(cv2_img, pred_classes[idx], box[0], cv2.FONT_HERSHEY_SIMPLEX,
-                        3, (0, 255, 0), thickness=3)  # Write the prediction class
-
-    return cv2_img, pred_boxes
-
-
-def get_pose_estimation_prediction(pose_model, image, center, scale):
-    rotation = 0
-
-    # pose estimation transformation
-    trans = get_affine_transform(center, scale, rotation, cfg.MODEL.IMAGE_SIZE)
-    model_input = cv2.warpAffine(
-        image,
-        trans,
-        (int(cfg.MODEL.IMAGE_SIZE[0]), int(cfg.MODEL.IMAGE_SIZE[1])),
-        flags=cv2.INTER_LINEAR)
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-    ])
-
-    # pose estimation inference
-    model_input = transform(model_input).unsqueeze(0)
-    # switch to evaluate mode
-    pose_model.eval()
-    with torch.no_grad():
-        # compute output heatmap
-        output = pose_model(model_input)
-        preds, _ = get_final_preds(
-            cfg,
-            output.clone().cpu().numpy(),
-            np.asarray([center]),
-            np.asarray([scale]))
-
-        return preds
-
-
-def box_to_center_scale(box, model_image_width, model_image_height):
-    """convert a box to center,scale information required for pose transformation
-    Parameters
-    ----------
-    box : list of tuple
-        list of length 2 with two tuples of floats representing
-        bottom left and top right corner of a box
-    model_image_width : int
-    model_image_height : int
-
-    Returns
-    -------
-    (numpy array, numpy array)
-        Two numpy arrays, coordinates for the center of the box and the scale of the box
-    """
-    center = np.zeros((2), dtype=np.float32)
-
-    bottom_left_corner = box[0]
-    top_right_corner = box[1]
-    box_width = top_right_corner[0]-bottom_left_corner[0]
-    box_height = top_right_corner[1]-bottom_left_corner[1]
-    bottom_left_x = bottom_left_corner[0]
-    bottom_left_y = bottom_left_corner[1]
-    center[0] = bottom_left_x + box_width * 0.5
-    center[1] = bottom_left_y + box_height * 0.5
-
-    aspect_ratio = model_image_width * 1.0 / model_image_height
-    pixel_std = 200
-
-    if box_width > aspect_ratio * box_height:
-        box_height = box_width * 1.0 / aspect_ratio
-    elif box_width < aspect_ratio * box_height:
-        box_width = box_height * aspect_ratio
-    scale = np.array(
-        [box_width * 1.0 / pixel_std, box_height * 1.0 / pixel_std],
-        dtype=np.float32)
-    if center[0] != -1:
-        scale = scale * 1.25
-
-    return center, scale
+from common import COCO_KEYPOINT_INDEXES
+from common import COCO_INSTANCE_CATEGORY_NAMES
+from common import get_person_detection_boxes
+from common import get_pose_estimation_prediction
+from common import box_to_center_scale
 
 
 def prepare_output_dirs(prefix='/output/'):
-    frame_dir = prefix+'frames/'
     pose_dir = prefix+'poses/'
     box_dir = prefix+'boxes/'
-    if os.path.exists(frame_dir) and os.path.isdir(frame_dir):
-        shutil.rmtree(frame_dir)
     if os.path.exists(pose_dir) and os.path.isdir(pose_dir):
         shutil.rmtree(pose_dir)
     if os.path.exists(box_dir) and os.path.isdir(box_dir):
         shutil.rmtree(box_dir)
-    os.makedirs(frame_dir, exist_ok=True)
     os.makedirs(pose_dir, exist_ok=True)
     os.makedirs(box_dir, exist_ok=True)
-    return frame_dir, pose_dir, box_dir
+    return pose_dir, box_dir
 
 
 def parse_args():
@@ -184,6 +52,7 @@ def parse_args():
     parser.add_argument('--videoFile', type=str, required=True)
     parser.add_argument('--outputDir', type=str, default='/output/')
     parser.add_argument('--inferenceFps', type=int, default=10)
+    parser.add_argument('--writeBoxFrames', action='store_true')
 
     parser.add_argument('opts',
                         help='Modify config options using the command-line',
@@ -208,7 +77,7 @@ def main():
 
     args = parse_args()
     update_config(cfg, args)
-    frame_dir, pose_dir, box_dir = prepare_output_dirs(args.outputDir)
+    pose_dir, box_dir = prepare_output_dirs(args.outputDir)
     csv_output_filename = args.outputDir+'pose-data.csv'
     csv_output_rows = []
 
@@ -235,7 +104,7 @@ def main():
         exit()
     every_nth_frame = round(fps/args.inferenceFps)
 
-    success, image = vidcap.read()
+    success, image_bgr = vidcap.read()
     count = 0
 
     while success:
@@ -243,30 +112,34 @@ def main():
             count += 1
             continue
 
+        image = image_bgr[:, :, [2, 1, 0]]
         count_str = str(count).zfill(32)
-        frame_local = frame_dir+'frame%s.jpg' % count_str
-        cv2.imwrite(frame_local, image)
 
         # object detection box
-        box_img, boxes = get_person_detection_boxes(box_model, image.copy(), threshold=0.8)
-        cv2.imwrite(box_dir+'box%s.jpg' % count_str, box_img)
-        box = boxes[0]  # assume there is only 1 person
-        center, scale = box_to_center_scale(box, cfg.MODEL.IMAGE_SIZE[0], cfg.MODEL.IMAGE_SIZE[1])
+        pred_boxes = get_person_detection_boxes(box_model, image, threshold=0.8)
+        if args.writeBoxFrames:
+            image_bgr_box = image_bgr.copy()
+            for box in pred_boxes:
+                cv2.rectangle(image_bgr_box, box[0], box[1], color=(0, 255, 0),
+                              thickness=3)  # Draw Rectangle with the coordinates
+            cv2.imwrite(box_dir+'box%s.jpg' % count_str, image_bgr_box)
 
         # pose estimation
-        preds = get_pose_estimation_prediction(pose_model, image, center, scale)
+        box = pred_boxes[0]  # assume there is only 1 person
+        center, scale = box_to_center_scale(box, cfg.MODEL.IMAGE_SIZE[0], cfg.MODEL.IMAGE_SIZE[1])
+        pose_preds = get_pose_estimation_prediction(pose_model, image, center, scale)
 
-        new_csv_row = [count]
-        for idx, mat in enumerate(preds[0]):
+        new_csv_row = []
+        for idx, mat in enumerate(pose_preds[0]):
             x_coord, y_coord = int(mat[0]), int(mat[1])
-            cv2.circle(image, (x_coord, y_coord), 4, (255, 0, 0), 2)
+            cv2.circle(image_bgr, (x_coord, y_coord), 4, (255, 0, 0), 2)
             new_csv_row.extend([x_coord, y_coord])
 
         csv_output_rows.append(new_csv_row)
-        cv2.imwrite(pose_dir+'pose%s.jpg' % count_str, image)
+        cv2.imwrite(pose_dir+'pose%s.jpg' % count_str, image_bgr)
 
         # get next frame
-        success, image = vidcap.read()
+        success, image_bgr = vidcap.read()
         count += 1
 
     # write csv
@@ -280,7 +153,7 @@ def main():
         csvwriter.writerows(csv_output_rows)
 
     pose_dir = '/output/poses/'
-    os.system("ffmpeg -pattern_type glob -i '"
+    os.system("ffmpeg -y -pattern_type glob -i '"
               + pose_dir
               + "/*.jpg' -c:v libx264 -vf fps="
               + str(args.inferenceFps)+" -pix_fmt yuv420p /output/movie.mp4")
