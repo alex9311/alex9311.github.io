29d791ef049d79b9fb42ff25382d03a3ea9901f0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 21:07:47 +0000

    Adding tarfile member sanitization to extractall()

--- code-projects/game-of-thrones-lda/train_lda.py
+++ code-projects/game-of-thrones-lda/train_lda.py
@@ -93,7 +93,26 @@ def main():
     model_object = os.path.join(prefix, 'output', training_job_name, 'output', model_fname)
     boto3.Session().resource('s3').Bucket(bucket).Object(model_object).download_file(fname)
     with tarfile.open(fname) as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+        	
+        	abs_directory = os.path.abspath(directory)
+        	abs_target = os.path.abspath(target)
+        
+        	prefix = os.path.commonprefix([abs_directory, abs_target])
+        	
+        	return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+        	for member in tar.getmembers():
+        		member_path = os.path.join(path, member.name)
+        		if not is_within_directory(path, member_path):
+        			raise Exception("Attempted Path Traversal in Tar File")
+        
+        	tar.extractall(path, members, numeric_owner=numeric_owner) 
+        	
+        
+        safe_extract(tar)
     print('Downloaded and extracted model tarball: {}'.format(model_object))
 
     # obtain the model file
